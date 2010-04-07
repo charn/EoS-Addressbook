8c32f23db835cf18161bd2ca5c60e05b05c27048
Juha Louhiranta <charni@gmail.com>
2010-04-07 19:09:57 +0300

    Kontrolleri aloitettu ja sille tehty testi.

---
+++ src/test/controller/AddressbookControllerTest.java
@@ -0,0 +1,28 @@
+package test.controller;
+
+import static org.junit.Assert.*;
+
+import main.controller.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AddressbookControllerTest {
+
+	private AddressbookController controller;
+	
+	@Before
+	public void setUp() throws Exception {
+		this.controller = new AddressbookController();
+	}
+
+//	@After
+//	public void tearDown() throws Exception {
+//	}
+
+	@Test public void create_new_addressbook_controller() {
+		
+	}
+
+}
