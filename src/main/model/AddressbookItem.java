75cbfeabdaabb7c61fb7abbfb3f3f9d87342ab13
T.Oittinen <toittine@sombrero.xrayphys.helsinki.fi>
2010-04-09 10:32:57 +0300

    Setters updated

--- src/main/model/AddressbookItem.java
+++ src/main/model/AddressbookItem.java
@@ -49,4 +49,60 @@ public class AddressbookItem {
 	public String getAddress(){
 		return this.address;
 	}
+
+	/**
+	 * @param maxId the maxId to set
+	 */
+	public static void setMaxId(int maxId)
+	{
+		AddressbookItem.maxId = maxId;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+
+	/**
+	 * @param firstname the firstname to set
+	 */
+	public void setFirstname(String firstname)
+	{
+		this.firstname = firstname;
+	}
+
+	/**
+	 * @param lastname the lastname to set
+	 */
+	public void setLastname(String lastname)
+	{
+		this.lastname = lastname;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email)
+	{
+		this.email = email;
+	}
+
+	/**
+	 * @param phonenumber the phonenumber to set
+	 */
+	public void setPhonenumber(String phonenumber)
+	{
+		this.phonenumber = phonenumber;
+	}
+
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address)
+	{
+		this.address = address;
+	}
 }
