affa265d1add911d5fee66b4501070fa935b2382
Matti <mtnauha@mtnauha-desktop.(none)>
2010-04-22 20:39:50 +0300

    ToolTipit lisätty

--- src/main/view/GUI.java
+++ src/main/view/GUI.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
 import java.util.List;
 
 import javax.swing.JButton;
@@ -23,122 +24,142 @@ import main.model.AddressbookItem;
  * http://java.sun.com/docs/books/tutorial/uiswing/components/toplevel.html
  * 
  * @author mtnauha
- *
+ * 
  */
 
 public class GUI extends JFrame implements AddressbookView, ActionListener {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	private JTable contactInfoTable;
 	private ContactInfoTableModel tableModel;
-	
+
 	private JToolBar toolBar;
-	
+
 	private JPanel contentPane;
-	
+
 	private JButton addButton;
 	private JButton removeButton;
 
-	
 	private AddressbookController controller;
-	
+
 	public GUI(AddressbookController controller) {
-		
+
 		this.controller = controller;
-		
+
 		setTitle("Addressbook");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		//Luodaan sisältö paneeli johon voidaan lisätä tavaraa
+
+		// Luodaan sisältö paneeli johon voidaan lisätä tavaraa
 		contentPane = new JPanel(new BorderLayout());
-		
-		//Luodaan työkalupalkki ja nappulat siihen
+
+		// Luodaan työkalupalkki ja nappulat siihen
 		toolBar = new JToolBar("Still draggable");
-		
+
 		addButton = new JButton("Add");
 		removeButton = new JButton("Remove");
-		
+
 		addButton.addActionListener(this);
 		removeButton.addActionListener(this);
 
-		
 		toolBar.add(addButton);
 		toolBar.add(removeButton);
-		
-		//Lisätään työkalupalkki sisältöpaneeliin
+
+		// Lisätään työkalupalkki sisältöpaneeliin
 		contentPane.add(toolBar, BorderLayout.PAGE_START);
-		
-		//Sisältöpaneeliin skrollattava ruutu, johon yhteystietotaulu
+
+		// Sisältöpaneeliin skrollattava ruutu, johon yhteystietotaulu
 		JScrollPane scrollPane = new JScrollPane(initContactInfoTable());
 		contentPane.add(scrollPane, BorderLayout.CENTER);
-		
+
 		setResizable(true);
-		setMinimumSize(new Dimension(300,300));
+		setMinimumSize(new Dimension(300, 300));
 		setContentPane(contentPane);
 		//
 		// Set our main menu
 		// 
-	    setJMenuBar(new MainMenuBar(this, controller));
-		
+		setJMenuBar(new MainMenuBar(this, controller));
+
 	}
-	
-	public JTable initContactInfoTable(){
-		tableModel = new ContactInfoTableModel(this.controller);	
-		
-		contactInfoTable = new JTable(tableModel);
-		
+
+	public JTable initContactInfoTable() {
+		tableModel = new ContactInfoTableModel(this.controller);
+
+		contactInfoTable = new JTable(tableModel) {
+
+			// Implement table cell tool tips.
+			public String getToolTipText(MouseEvent e) {
+				String tip = null;
+				java.awt.Point p = e.getPoint();
+				int rowIndex = rowAtPoint(p);
+
+				tip = getValueAt(rowIndex, 0) + ", " + getValueAt(rowIndex, 1)
+						+ ", " + getValueAt(rowIndex, 2) + ", "
+						+ getValueAt(rowIndex, 3) + ", "
+						+ getValueAt(rowIndex, 4) + ", "
+						+ getValueAt(rowIndex, 5);
+
+				return tip;
+			}
+
+		};
+
 		contactInfoTable.setAutoCreateRowSorter(true);
-		
+
 		return contactInfoTable;
 	}
-	
+
 	public void updateAddressbook(List<AddressbookItem> model) {
 		tableModel.updateAddressbook(model);
-		
+
 	}
 
 	public void actionPerformed(ActionEvent e) {
-		
-		if(e.getSource() == addButton) {
-			//AddDialog dialog = new AddDialog(this, controller);
-			//dialog.pack();
+
+		if (e.getSource() == addButton) {
+			// AddDialog dialog = new AddDialog(this, controller);
+			// dialog.pack();
 			//                     
-			//dialog.setLocationRelativeTo(this);
-			//dialog.setVisible(true);
+			// dialog.setLocationRelativeTo(this);
+			// dialog.setVisible(true);
 
-            controller.addItem(new AddressbookItem());
-//            int addedRow = contactInfoTable.convertRowIndexToModel(tableModel.getRowCount() - 1);
+			controller.addItem(new AddressbookItem());
+			// int addedRow =
+			// contactInfoTable.convertRowIndexToModel(tableModel.getRowCount()
+			// - 1);
 
-//            System.out.println(addedRow);
-            
-//            contactInfoTable.changeSelection(addedRow, 0, true, false);	
-//            contactInfoTable.editCellAt(addedRow, 0);
-            contactInfoTable.requestFocusInWindow();
+			// System.out.println(addedRow);
+
+			// contactInfoTable.changeSelection(addedRow, 0, true, false);
+			// contactInfoTable.editCellAt(addedRow, 0);
+			contactInfoTable.requestFocusInWindow();
 		}
-		
-		else if(e.getSource() == removeButton) {
+
+		else if (e.getSource() == removeButton) {
 			int selectedRow = contactInfoTable.getSelectedRow();
-			
+
 			int nextToBeSelected = -1;
-			
-			if(selectedRow >= 0 && selectedRow != contactInfoTable.getEditingRow()) {
-				
+
+			if (selectedRow >= 0
+					&& selectedRow != contactInfoTable.getEditingRow()) {
+
 				// Selected row is the last on the list
-				if(selectedRow + 1 == contactInfoTable.getRowCount())
+				if (selectedRow + 1 == contactInfoTable.getRowCount())
 					nextToBeSelected = selectedRow - 1;
 				else
 					nextToBeSelected = selectedRow;
-				
-				int delete = contactInfoTable.convertRowIndexToModel(selectedRow);
+
+				int delete = contactInfoTable
+						.convertRowIndexToModel(selectedRow);
 				controller.removeItem(delete);
-				
+
 			}
 
-			if(nextToBeSelected != -1)
-				contactInfoTable.changeSelection(nextToBeSelected, 0, false, false);	
+			if (nextToBeSelected != -1)
+				contactInfoTable.changeSelection(nextToBeSelected, 0, false,
+						false);
 		}
-		
+
 	}
 
 }
